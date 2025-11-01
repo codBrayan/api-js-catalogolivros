770141b519438a5aa36bef4da25abcc482b10128
Syyyck <brayandelimapf@gmail.com>
2025-11-01 20:13:32 -0300

    style: Estilização do componente BookCard

--- src/components/BookCard/index.jsx
+++ src/components/BookCard/index.jsx
@@ -4,8 +4,8 @@ import './style.css';
 function BookCard({ title, author, coverImage }) {
   return (
     <div className="book-card-container">
-      <img src={coverImage} alt={`Capa do livro ${title}`} />
       <h3>{title}</h3>
+      <img src={coverImage} alt={`Capa do livro ${title}`} />
       <p>{author}</p>
     </div>
   );
